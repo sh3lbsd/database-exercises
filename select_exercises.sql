4bdea3be8f5b18a261c1917db0deabe003786830
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-04 12:28:20 -0500

    finished select-exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,21 +1,19 @@
 USE codeup_test_db;
 
-SELECT 'Pink Floyd Albums' AS 'Query One';
+SELECT name AS 'The name of all albums by Pink Floyd' FROM albums
+WHERE artist = 'Pink Floyd';
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' FROM albums
+WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
 
-SELECT release_date FROM albums WHERE name = 'SGT. Peppers''s Lonley Hearts Club BAnd was released';
+SELECT genre AS 'The genre for Nevermind' FROM albums
+WHERE name = 'Nevermind';
 
-SELECT 'The genre for Nevermind' AS 'Query Three';
+SELECT name AS 'Albums released in the 1990s' FROM albums
+WHERE release_date BETWEEN 1989 AND 2000;
 
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT name AS 'Albums that had less than 20 million certified sales' FROM albums
+WHERE sales < 20;
 
-SELECT 'Which albums were released in the 1990''s'AS 'Query Four';
-
-SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
-
-SELECT * FROM albums WHERE sales < 20;
-
-SELECT 'All Rock Albums' AS 'Query Six';
-
-SELECT * FROM albums WHERE genre = 'Rock';
+SELECT name AS 'Albums with a genre of Rock' FROM albums
+WHERE genre = 'Rock';
