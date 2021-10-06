e6b6021215f7baebb343cf68490b363d439f2a67
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-05 22:05:41 -0500

    fixed delete exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -3,8 +3,20 @@ USE codeup_test_db;
 DELETE FROM albums
 WHERE release_date > 1991;
 
+SELECT name AS 'Albums released after 1991'
+FROM albums
+WHERE release_date > 1991;
+
 DELETE FROM albums
 WHERE genre = 'disco';
 
-DELETE  FROM albums
-WHERE artist = 'Nirvana'
+SELECT name AS 'Albums with the genre ''disco'''
+FROM albums
+WHERE genre = 'disco';
+
+DELETE FROM albums
+WHERE artist = 'Nirvana';
+
+SELECT name AS 'Albums by ''Nirvana'''
+FROM albums
+WHERE artist = 'Nirvana';
