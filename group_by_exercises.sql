c3b9aa867d67b05f55fd934a967e13e0c19ce20f
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-05 22:52:09 -0500

    finished group_by_exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,27 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT DISTINCT first_name, last_name, COUNT(*)
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT DISTINCT COUNT(*), last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+
+
+SELECT COUNT(*) AS 'COUNT', gender
+FROM employees
+WHERE first_name = 'Irena' OR first_name  = 'Vidya' OR first_name = 'Maya'
+
+GROUP BY 'COUNT', gender, gender = 'M' AND 'COUNT', gender = 'F';
