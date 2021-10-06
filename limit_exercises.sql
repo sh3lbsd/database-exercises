d3bb2b267118ce73607a8954fb8750af325eec35
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-05 22:22:30 -0500

    fixed limit_exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,8 +7,12 @@ WHERE last_name LIKE 'Z%'
 ORDER BY last_name DESC
 LIMIT 10;
 
+SELECT DISTINCT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
 SELECT DISTINCT emp_no, salary
 FROM salaries
 ORDER BY salary DESC
 LIMIT 5 OFFSET 45;
-
