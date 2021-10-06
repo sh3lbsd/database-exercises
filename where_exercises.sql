319bd218d6ccaeeecc0547fba2a0034bf02478e8
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-05 22:10:13 -0500

    lightning fast where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,36 @@
 USE employees;
+
 DESCRIBE employees;
 
 SELECT first_name, last_name
 FROM employees
 WHERE first_name IN ('Irena','Vidya','Maya');
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE  'E%';
+
+SElECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name ='Vidya'
+   OR first_name ='Maya';
+
+SELECT * FROM employees
+WHERE gender ='m' AND (first_name LIKE 'Irena'
+    OR first_name LIKE 'Vidya'
+    OR first_name LIKE 'Maya');
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE  '%q%'
+  AND last_name NOT LIKE '%qu%';
